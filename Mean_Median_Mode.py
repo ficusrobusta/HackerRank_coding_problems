387716d11eb75840a0adfb8846de3e10ebdb8b41
ficusrobusta <70322151+ficusrobusta@users.noreply.github.com>
2021-01-26 13:17:47 +0000

    Update Mean_Median_Mode.py

--- Mean_Median_Mode.py
+++ Mean_Median_Mode.py
@@ -1,30 +1,30 @@
-Problem
-Objective 
-In this challenge, we practice calculating the mean, median, and mode. Check out the Tutorial tab for learning materials and an instructional video!
-Task 
-Given an array, , of  integers, calculate and print the respective mean, median, and mode on separate lines. If your array contains more than one modal value, choose the numerically smallest one.
-Note: Other than the modal value (which will always be an integer), your answers should be in decimal form, rounded to a scale of  decimal place (i.e., ,  format).
-The mean is . 
-The median is . 
-The mode is  because  occurs most frequently.
-Input Format
-The first line contains an integer, , the number of elements in the array. 
-The second line contains  space-separated integers that describe the array's elements
-, where  is the  element of the array.
-Output Format
-Print  lines of output in the following order:
-Print the mean on the first line to a scale of  decimal place (i.e., , ).
-Print the median on a new line, to a scale of  decimal place (i.e., , ).
-Print the mode on a new line. If more than one such value exists, print the numerically smallest one.
-Sample Input
-10
-64630 11735 14216 99233 14470 4978 73429 38120 51135 67060
-Sample Output
-43900.6
-44627.5
-4978
+# Problem
+# Objective 
+# In this challenge, we practice calculating the mean, median, and mode. Check out the Tutorial tab for learning materials and an instructional video!
+# Task 
+# Given an array, , of  integers, calculate and print the respective mean, median, and mode on separate lines. If your array contains more than one modal value, choose the numerically smallest one.
+# Note: Other than the modal value (which will always be an integer), your answers should be in decimal form, rounded to a scale of  decimal place (i.e., ,  format).
+# The mean is . 
+# The median is . 
+# The mode is  because  occurs most frequently.
+# Input Format
+# The first line contains an integer, , the number of elements in the array. 
+# The second line contains  space-separated integers that describe the array's elements
+# , where  is the  element of the array.
+# Output Format
+# Print  lines of output in the following order:
+# Print the mean on the first line to a scale of  decimal place (i.e., , ).
+# Print the median on a new line, to a scale of  decimal place (i.e., , ).
+# Print the mode on a new line. If more than one such value exists, print the numerically smallest one.
+# Sample Input
+# 10
+# 64630 11735 14216 99233 14470 4978 73429 38120 51135 67060
+# Sample Output
+# 43900.6
+# 44627.5
+# 4978
 
-# Solution
+# Solution UNFINISHED
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 #import sys
 N = int(input())
@@ -51,4 +51,3 @@ else:
 print (mean)
 print(median)
 print(mode)
-
