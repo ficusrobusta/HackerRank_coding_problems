830b09cf0e25efed72a7662c41b3f9407ee2c87c
ficusrobusta <70322151+ficusrobusta@users.noreply.github.com>
2021-01-26 13:10:44 +0000

    Create Sales_By_Match.py

---
+++ Sales_By_Match.py
@@ -0,0 +1,53 @@
+Problem
+There is a large pile of socks that must be paired by color. Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
+There is one pair of color  and one of color . There are three odd socks left, one of each color. The number of pairs is .
+Function Description
+Complete the sockMerchant function in the editor below.
+sockMerchant has the following parameter(s):
+int n: the number of socks in the pile
+int ar[n]: the colors of each sock
+Returns
+int: the number of pairs
+Input Format
+The first line contains an integer , the number of socks represented in . 
+The second line contains  space-separated integers, , the colors of the socks in the pile.
+Sample Input
+STDIN                       Function
+-----                       --------
+9                           n = 9
+10 20 20 10 10 30 50 10 20  ar = [10, 20, 20, 10, 10, 30, 50, 10, 20]
+Sample Output
+3
+
+Solution
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the sockMerchant function below.
+from itertools import groupby
+n = int(raw_input())
+c = map(int, raw_input().split())
+
+ans = 0
+for val in [len(list(group)) for key, group in groupby(sorted(c))]:
+    ans = ans + val/2
+print ans
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
