a5f8199be93ea1ecf07a731fc57be8c33dd9e072
ficusrobusta <70322151+ficusrobusta@users.noreply.github.com>
2021-02-05 21:00:30 +0000

    Update Arrays.py

--- Arrays.py
+++ Arrays.py
@@ -1,27 +1,27 @@
-Problem
-Objective 
-Today, we will learn about the Array data structure. Check out the Tutorial tab for learning materials and an instructional video.
-Task 
-Given an array, , of  integers, print 's elements in reverse order as a single line of space-separated numbers.
-Example
+# Problem
+# Objective 
+# Today, we will learn about the Array data structure. Check out the Tutorial tab for learning materials and an instructional video.
+# Task 
+# Given an array, , of  integers, print 's elements in reverse order as a single line of space-separated numbers.
+# Example
 
-Print 4 3 2 1. Each integer is separated by one space.
-Input Format
-The first line contains an integer,  (the size of our array). 
-The second line contains  space-separated integers that describe array 's elements.
-Constraints
-Constraints
+# Print 4 3 2 1. Each integer is separated by one space.
+# Input Format
+# The first line contains an integer,  (the size of our array). 
+# The second line contains  space-separated integers that describe array 's elements.
+# Constraints
+# Constraints
 
-, where  is the  integer in the array.
-Output Format
-Print the elements of array  in reverse order as a single line of space-separated numbers.
-Sample Input
-4
-1 4 3 2
-Sample Output
-2 3 4 1
+# , where  is the  integer in the array.
+# Output Format
+# Print the elements of array  in reverse order as a single line of space-separated numbers.
+# Sample Input
+# 4
+# 1 4 3 2
+# Sample Output
+# 2 3 4 1
 
-Solution
+# Solution
 
 #!/bin/python3
 
