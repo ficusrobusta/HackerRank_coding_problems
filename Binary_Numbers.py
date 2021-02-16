9c3b9e6bb74e108a9e0b74bf0c128405de8913e3
ficusrobusta <70322151+ficusrobusta@users.noreply.github.com>
2021-02-16 15:21:37 +0000

    Update Binary_Numbers.py

--- Binary_Numbers.py
+++ Binary_Numbers.py
@@ -1,25 +1,25 @@
-Problem
-Task 
-Given a base- integer, , convert it to binary (base-). Then find and print the base- integer denoting the maximum number of consecutive 's in 's binary representation. When working with different bases, it is common to show the base as a subscript.
-Example 
+# Problem
+# Task 
+# Given a base- integer, , convert it to binary (base-). Then find and print the base- integer denoting the maximum number of consecutive 's in 's binary representation. When working with different bases, it is common to show the base as a subscript.
+# Example 
 
-The binary representation of  is . In base , there are  and  consecutive ones in two groups. Print the maximum, .
-Input Format
-A single integer, .
-Constraints
+# The binary representation of  is . In base , there are  and  consecutive ones in two groups. Print the maximum, .
+# Input Format
+# A single integer, .
+# Constraints
 
-Output Format
-Print a single base- integer that denotes the maximum number of consecutive 's in the binary representation of .
-Sample Input 1
-5
-Sample Output 1
-1
-Sample Input 2
-13
-Sample Output 2
-2
+# Output Format
+# Print a single base- integer that denotes the maximum number of consecutive 's in the binary representation of .
+# Sample Input 1
+# 5
+# Sample Output 1
+# 1
+# Sample Input 2
+# 13
+# Sample Output 2
+# 2
 
-Solution 
+# Solution 
 
 #!/bin/python3
 
